98fcd551824a4161272eab22f544ebb5363b6c95
Dushyant Bhalgami <dushyantbhalgami@gmail.com>
2019-07-16 11:19:14 +0530

    added circleci and build.sh

--- config/default.js
+++ config/default.js
@@ -7,19 +7,21 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   API_VERSION: process.env.API_VERSION || '/v5',
   AUTH_SECRET: process.env.AUTH_SECRET || 'mysecret',
-  VALID_ISSUERS: process.env.VALID_ISSUERS ? process.env.VALID_ISSUERS.replace(/\\"/g, '') : '["https://api.topcoder.com"]',
+  VALID_ISSUERS: process.env.VALID_ISSUERS
+    ? process.env.VALID_ISSUERS.replace(/\\"/g, '')
+    : '["https://api.topcoder-dev.com", "https://api.topcoder.com","https://topcoder-dev.auth0.com/"]',
 
   AMAZON: {
-    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID || 'FAKE_ACCESS_KEY',
-    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY || 'FAKE_SECRET_ACCESS_KEY',
-    AWS_REGION: process.env.AWS_REGION || 'ap-northeast-1',
-    IS_LOCAL_DB: process.env.IS_LOCAL_DB ? process.env.IS_LOCAL_DB === 'true' : true,
-    // DYNAMODB_URL is required if IS_LOCAL_DB is true
-    DYNAMODB_URL: process.env.DYNAMODB_URL || 'http://localhost:8000',
-    DYNAMODB_READ_CAPACITY_UNITS: process.env.DYNAMODB_READ_CAPACITY_UNITS || 10,
-    DYNAMODB_WRITE_CAPACITY_UNITS: process.env.DYNAMODB_WRITE_CAPACITY_UNITS || 5,
+    AWS_REGION: process.env.AWS_REGION || 'us-east-1',
+    DYNAMODB_READ_CAPACITY_UNITS: process.env.DYNAMODB_READ_CAPACITY_UNITS
+      ? Number(process.env.DYNAMODB_READ_CAPACITY_UNITS)
+      : 10,
+    DYNAMODB_WRITE_CAPACITY_UNITS: process.env.DYNAMODB_WRITE_CAPACITY_UNITS
+      ? Number(process.env.DYNAMODB_WRITE_CAPACITY_UNITS)
+      : 5,
+
     // table name is model name
-    DYNAMODB_SCORE_SYSTEM_TABLE: process.env.DYNAMODB_SCORE_SYSTEM_TABLE || 'scoreSystems',
+    DYNAMODB_SCORE_SYSTEM_TABLE: process.env.DYNAMODB_SCORE_SYSTEM_TABLE || 'score_systems',
     DYNAMODB_SCORECARD_TABLE: process.env.DYNAMODB_SCORECARD_TABLE || 'scorecards'
   }
-}
+};
--- src/common/helper.js
+++ src/common/helper.js
@@ -1,30 +1,28 @@
 /**
  * This file defines helper methods
  */
-const _ = require('lodash')
-const config = require('config')
-const AWS = require('aws-sdk')
-const models = require('../models')
-const errors = require('./errors')
+const _ = require('lodash');
+const config = require('config');
+const AWS = require('aws-sdk');
+const models = require('../models');
+const errors = require('./errors');
 
 // AWS DynamoDB instance
-let dbInstance
+let dbInstance;
 
 AWS.config.update({
-  accessKeyId: config.AMAZON.AWS_ACCESS_KEY_ID,
-  secretAccessKey: config.AMAZON.AWS_SECRET_ACCESS_KEY,
   region: config.AMAZON.AWS_REGION
-})
+});
 
 /**
  * Wrap async function to standard express function
  * @param {Function} fn the async function
  * @returns {Function} the wrapped function
  */
-function wrapExpress (fn) {
-  return function (req, res, next) {
-    fn(req, res, next).catch(next)
-  }
+function wrapExpress(fn) {
+  return function(req, res, next) {
+    fn(req, res, next).catch(next);
+  };
 }
 
 /**
@@ -32,36 +30,32 @@ function wrapExpress (fn) {
  * @param obj the object (controller exports)
  * @returns {Object|Array} the wrapped object
  */
-function autoWrapExpress (obj) {
+function autoWrapExpress(obj) {
   if (_.isArray(obj)) {
-    return obj.map(autoWrapExpress)
+    return obj.map(autoWrapExpress);
   }
   if (_.isFunction(obj)) {
     if (obj.constructor.name === 'AsyncFunction') {
-      return wrapExpress(obj)
+      return wrapExpress(obj);
     }
-    return obj
+    return obj;
   }
   _.each(obj, (value, key) => {
-    obj[key] = autoWrapExpress(value)
-  })
-  return obj
+    obj[key] = autoWrapExpress(value);
+  });
+  return obj;
 }
 
 /**
  * Get DynamoDB Connection Instance
  * @return {Object} DynamoDB Connection Instance
  */
-function getDb () {
+function getDb() {
   // cache it for better performance
   if (!dbInstance) {
-    if (config.AMAZON.IS_LOCAL_DB) {
-      dbInstance = new AWS.DynamoDB({ endpoint: config.AMAZON.DYNAMODB_URL })
-    } else {
-      dbInstance = new AWS.DynamoDB()
-    }
+    dbInstance = new AWS.DynamoDB();
   }
-  return dbInstance
+  return dbInstance;
 }
 
 /**
@@ -69,17 +63,17 @@ function getDb () {
  * @param     {object} model Table structure in JSON format
  * @return    {promise} the result
  */
-async function createTable (model) {
-  const db = getDb()
+async function createTable(model) {
+  const db = getDb();
   return new Promise((resolve, reject) => {
     db.createTable(model, (err, data) => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve(data)
+        resolve(data);
       }
-    })
-  })
+    });
+  });
 }
 
 /**
@@ -87,20 +81,20 @@ async function createTable (model) {
  * @param     {String} tableName Name of the table to be deleted
  * @return    {promise} the result
  */
-async function deleteTable (tableName) {
-  const db = getDb()
+async function deleteTable(tableName) {
+  const db = getDb();
   const item = {
     TableName: tableName
-  }
+  };
   return new Promise((resolve, reject) => {
     db.deleteTable(item, (err, data) => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve(data)
+        resolve(data);
       }
-    })
-  })
+    });
+  });
 }
 
 /**
@@ -110,19 +104,22 @@ async function deleteTable (tableName) {
  * @param {Class} ErrorClass the error class
  * @returns {Object} found record
  */
-async function getById (modelName, id, ErrorClass) {
-  const ErrCls = ErrorClass || errors.NotFoundError
+async function getById(modelName, id, ErrorClass) {
+  const ErrCls = ErrorClass || errors.NotFoundError;
   return new Promise((resolve, reject) => {
-    models[modelName].query('id').eq(id).exec((err, result) => {
-      if (err) {
-        reject(err)
-      } else if (result.length > 0) {
-        resolve(result[0])
-      } else {
-        reject(new ErrCls(`${modelName} with id: ${id} doesn't exist`))
-      }
-    })
-  })
+    models[modelName]
+      .query('id')
+      .eq(id)
+      .exec((err, result) => {
+        if (err) {
+          reject(err);
+        } else if (result.length > 0) {
+          resolve(result[0]);
+        } else {
+          reject(new ErrCls(`${modelName} with id: ${id} doesn't exist`));
+        }
+      });
+  });
 }
 
 /**
@@ -131,17 +128,17 @@ async function getById (modelName, id, ErrorClass) {
  * @param {Object} data The create data object
  * @returns {Object} created entity
  */
-async function create (modelName, data) {
+async function create(modelName, data) {
   return new Promise((resolve, reject) => {
-    const dbItem = new models[modelName](data)
-    dbItem.save((err) => {
+    const dbItem = new models[modelName](data);
+    dbItem.save(err => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve(dbItem)
+        resolve(dbItem);
       }
-    })
-  })
+    });
+  });
 }
 
 /**
@@ -150,33 +147,33 @@ async function create (modelName, data) {
  * @param {Object} data The updated data object
  * @returns {Object} updated entity
  */
-async function update (dbItem, data) {
-  _.assignIn(dbItem, data)
+async function update(dbItem, data) {
+  _.assignIn(dbItem, data);
   return new Promise((resolve, reject) => {
-    dbItem.save((err) => {
+    dbItem.save(err => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve(dbItem)
+        resolve(dbItem);
       }
-    })
-  })
+    });
+  });
 }
 
 /**
  * Remove item in database
  * @param {Object} dbItem The Dynamo database item to remove
  */
-async function remove (dbItem) {
+async function remove(dbItem) {
   return new Promise((resolve, reject) => {
-    dbItem.delete((err) => {
+    dbItem.delete(err => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve(dbItem)
+        resolve(dbItem);
       }
-    })
-  })
+    });
+  });
 }
 
 /**
@@ -185,16 +182,16 @@ async function remove (dbItem) {
  * @param {Object} scanParams The scan parameters object
  * @returns {Array} found records
  */
-async function scan (modelName, scanParams) {
+async function scan(modelName, scanParams) {
   return new Promise((resolve, reject) => {
     models[modelName].scan(scanParams).exec((err, result) => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve(result.count === 0 ? [] : result)
+        resolve(result.count === 0 ? [] : result);
       }
-    })
-  })
+    });
+  });
 }
 
 /**
@@ -203,11 +200,11 @@ async function scan (modelName, scanParams) {
  * @param {String} s the matching string to find
  * @returns {Boolean} whether s is part of value, case-insensitive
  */
-function matchString (value, s) {
+function matchString(value, s) {
   if (!value) {
-    return false
+    return false;
   }
-  return value.toLowerCase().indexOf(s.toLowerCase()) >= 0
+  return value.toLowerCase().indexOf(s.toLowerCase()) >= 0;
 }
 
 module.exports = {
@@ -221,4 +218,4 @@ module.exports = {
   remove,
   scan,
   matchString
-}
+};
